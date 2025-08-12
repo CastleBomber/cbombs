d061681d37577e9f7b26c53b3687bd96c9412942
CastleBomber <richie.ucsb@gmail.com>
2025-08-11 17:01:54 -0700

    x

--- html-stuff/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.querySelector(".nav-toggle");
-  const nav = document.querySelector("nav");
-
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-  });
-});
